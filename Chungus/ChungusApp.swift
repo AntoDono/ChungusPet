8987f3917b1a0a139250b2a8b00d368d969d0c56
AntoDono <anthony20151128@gmail.com>
2026-02-23 01:36:16 -0500

    Initial Commit

---
+++ Chungus/ChungusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChungusApp.swift
+//  Chungus
+//
+//  Created by Anto Z on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct ChungusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
